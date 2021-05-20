1f33daf109ea9b19aadb00c4d5decb85a9514d5c
joaovictorneto <joaovictorneto@email.com>
2021-05-20 18:49:54 -0300

    180. Selecionando Refeições por Categoria

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
@@ -6,12 +7,20 @@ class CategoriesMealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context).settings.arguments as Category;
 
+    // Sempre retorna verdadeiro (caso dê certo)
+    final categoryMeals = DUMMY_MEALS.where((element) {
+      return element.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Center(
-        child: Text('Receitas por Categoria ${category.id}'),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
       ),
     );
   }
