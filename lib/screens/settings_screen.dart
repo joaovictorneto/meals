1134ce55523426139bd415782f3263e91a349863
joaovictorneto <joaovictorneto@email.com>
2021-05-22 12:50:35 -0300

    193. Tela de Configurações

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:meals/components/main_drawer.dart';
+import 'package:meals/models/settings.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  var settings = Settings();
+
+  Widget _criarSwtich(
+    String titulo,
+    String subtitulo,
+    bool valor,
+    Function onChanged,
+  ) {
+    return SwitchListTile.adaptive(
+      title: Text(titulo),
+      subtitle: Text(subtitulo),
+      value: valor,
+      onChanged: onChanged,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,8 +31,44 @@ class SettingsScreen extends StatelessWidget {
         title: Text('Configurações'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('Configurações'),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child:
+                Text('Filtros', style: Theme.of(context).textTheme.headline6),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                _criarSwtich(
+                  'Sem glúten',
+                  'Exibir somente alimentos sem glúten',
+                  settings.isGlutenFree,
+                  (valor) => setState(() => settings.isGlutenFree = valor),
+                ),
+                _criarSwtich(
+                  'Sem lactose',
+                  'Exibir somente alimentos sem lactose.',
+                  settings.isLactoseFree,
+                  (valor) => setState(() => settings.isLactoseFree = valor),
+                ),
+                _criarSwtich(
+                  'Vegana',
+                  'Exibir somente alimentos veganos.',
+                  settings.isVegan,
+                  (valor) => setState(() => settings.isVegan = valor),
+                ),
+                _criarSwtich(
+                  'Vegetariana',
+                  'Exibir somente alimentos vegetarianos.',
+                  settings.isVegetarian,
+                  (valor) => setState(() => settings.isVegetarian = valor),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
