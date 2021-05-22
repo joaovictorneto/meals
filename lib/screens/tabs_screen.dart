02ae2db4fecd24353a2f6fbc57e93a3ac33f0522
joaovictorneto <joaovictorneto@email.com>
2021-05-22 10:31:16 -0300

    188. Adicionar TabBar na Parte Inferior

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,33 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/favorite_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _indiceTelaSelecionada = 0;
+  final List<Map<String, Object>> _telas = [
+    {'titulo': 'Categorias', 'tela': CategoriesScreen()},
+    {'titulo': 'Favoritas', 'tela': FavoriteScreen()},
+  ];
+
+  _selecionarTela(int indice) {
+    setState(() {
+      _indiceTelaSelecionada = indice;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Partiu cozinhar?'),
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categorias',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favoritas',
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_telas[_indiceTelaSelecionada]['titulo']),
+      ),
+      body: _telas[_indiceTelaSelecionada]['tela'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selecionarTela,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _indiceTelaSelecionada,
+        //type: BottomNavigationBarType.shifting, -> Se ativar, add backgroudcolor nos bottomnavigationbaritem
+        items: [
+          BottomNavigationBarItem(
+            // backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            label: 'Categorias',
           ),
-        ),
-        body: TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavoriteScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            // backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.favorite),
+            label: 'Favoritas',
+          ),
+        ],
       ),
     );
   }
